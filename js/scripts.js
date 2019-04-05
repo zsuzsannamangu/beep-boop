8bbfb34a57bbf9ccb382a1b7b66fe7d47763b0ed
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-04-05 14:32:09 -0700

    make additional button in html for reverse order, write js code for reverse order option

--- js/scripts.js
+++ js/scripts.js
@@ -28,15 +28,44 @@ function makeArray(number) {
   }
 }
 
-// var myNumber = 8732;
-// console.log(makeArray(myNumber));
+function reverseOrder(number) {
+  var output = []
+  var numberString = number.toString();
+  if (numberString.includes("3")) {
+    return 'I am sorry Dave, I am afraid I cannot do that. <img src="img/icant.png" alt="This is an image of I cannot.">'
+  } else if (numberString.includes("2")) {
+    return 'Boop! <img src="img/boop2.jpg" alt="This is an image of boop.">'
+  } else if (numberString.includes("1")) {
+    return 'Beep! <img src="img/beep.png" alt="This is an image of beep.">'
+  } else {
+    for (var i = 0; i <= number; i++) {
+      output.push(changeNumber(i));
+    }
+    return output.reverse();
+  }
+}
+
+// var myNumber = 78;
+// console.log(reverseOrder(myNumber));
 
 
 $(document).ready(function(){
-  $("#form").submit(function(event) {
+  $("#button1").click(function(event) {
     event.preventDefault();
     var number = parseInt($("#enterNumber").val());
     var output = makeArray(number);
+    $(".yourResult").html(output);
+
+    $("#result").show();
+
+  });
+});
+
+$(document).ready(function(){
+  $("#button2").click(function(event) {
+    event.preventDefault();
+    var number = parseInt($("#enterNumber").val());
+    var output = reverseOrder(number);
     $(".yourResult").html(output);
 
     $("#result").show();
