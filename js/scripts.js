dc04b8c637580fe034746a64a352260169eb5dfc
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-04-05 10:06:19 -0700

    write business logic in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
+function changeNumber(number) {
+  var numberString = number.toString();
+  if (numberString.includes("3")) {
+    return "I'm sorry, Dave. I'm afraid I can't do that."
+  } else if (numberString.includes("2")) {
+    return "Boop"
+  } else if (numberString.includes("1")) {
+    return "Beep"
+  } else {
+    return number;
+  }
+}
 
 function makeArray(number) {
   var output = [];
   for (var i = 0; i <= number; i++) {
-    output.push(i);
+    output.push(changeNumber(i));
   }
   return output;
 }
 
-var myNumber = 124;
+var myNumber = 23;
 console.log(makeArray(myNumber));
